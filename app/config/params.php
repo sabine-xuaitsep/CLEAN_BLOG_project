8d00735cedcc1801d1a3d40207e32ce282948554
sabine-xuaitsep <sabinepestiaux@gmail.com>
2021-09-14 13:55:11 +0200

    MVC structure implementation

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+  ./app/config/params.php
+*/
+
+// Connection settings 
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cleanblog_2017');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
